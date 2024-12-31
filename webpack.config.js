c88203372da1a25f2bc0352ce628b12e4ea5a971
Sam DeVore <sdevore@gmail.com>
2024-12-31 15:44:24 -0700

    🎨 [wp-scripts] lint and format fixes

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,15 @@ const path = require( 'path' );
 module.exports = {
 	...defaultConfig,
 	entry: {
-		'multi-block-editor': [ path.resolve( __dirname, 'src/multi-block-editor.js' ) ],
-		'multi-block-frontend': [ path.resolve( __dirname, 'src/multi-block-frontend.js' ) ],
+		'multi-block-editor': [
+			path.resolve( __dirname, 'src/multi-block-editor.js' ),
+		],
+		'multi-block-frontend': [
+			path.resolve( __dirname, 'src/multi-block-frontend.js' ),
+		],
 	},
 	output: {
-		path: path.resolve( __dirname, 'build' ), filename: '[name].js',
+		path: path.resolve( __dirname, 'build' ),
+		filename: '[name].js',
 	},
 };
